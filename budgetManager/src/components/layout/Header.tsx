e162763066e03a25a4c360062785bdb3a9ffb560
cloNoey <tmddus0723@snu.ac.kr>
2025-10-19 18:44:01 +0900

    feature: date navigation

--- budgetManager/src/components/layout/Header.tsx
+++ budgetManager/src/components/layout/Header.tsx
@@ -1,3 +1,4 @@
+import { useState } from 'react';
 import '../../styles/components/Header.css'
 import docIcon from '../../icons/doc.svg'
 import calendarIcon from '../../icons/calendar.svg'
@@ -6,8 +7,24 @@ import chevronLeftIcon from '../../icons/chevron-left.svg'
 import chevronRightIcon from '../../icons/chevron-right.svg'
 
 export default function Header() {
-  // 현재 날짜 정보
-  const currentDate = new Date();
+  // useState로 현재 날짜 상태 관리
+  const [currentDate, setCurrentDate] = useState(new Date());
+  
+  // 이전 날로 이동하는 함수
+  const handlePreviousDay = () => {
+    const newDate = new Date(currentDate);
+    newDate.setDate(newDate.getDate() - 1);
+    setCurrentDate(newDate);
+  };
+  
+  // 다음 날로 이동하는 함수
+  const handleNextDay = () => {
+    const newDate = new Date(currentDate);
+    newDate.setDate(newDate.getDate() + 1);
+    setCurrentDate(newDate);
+  };
+
+  // 현재 날짜 정보 계산
   const year = currentDate.getFullYear();
   const month = currentDate.getMonth() + 1; // 0부터 시작하므로 +1
   const day = currentDate.getDate();
@@ -29,9 +46,19 @@ export default function Header() {
         <div className="monthYear">
           <div className="year">{year}</div>
           <div className="dayContainer">
-            <img src={chevronLeftIcon} alt="previous day" className="chevronIcon" />
+            <img 
+              src={chevronLeftIcon} 
+              alt="previous day" 
+              className="chevronIcon" 
+              onClick={handlePreviousDay}
+            />
             <div className="day">{day}</div>
-            <img src={chevronRightIcon} alt="next day" className="chevronIcon" />
+            <img 
+              src={chevronRightIcon} 
+              alt="next day" 
+              className="chevronIcon" 
+              onClick={handleNextDay}
+            />
           </div>
           <div className="month">{monthName}</div>
         </div>
